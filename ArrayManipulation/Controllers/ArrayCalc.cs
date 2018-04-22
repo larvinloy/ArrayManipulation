2eeb53c144a18a8bd89a76d5fd5cc04905d2716e
Loy Larvin <larvinloy@gmail.com>
2018-04-22 22:21:01 +1000

    Init proj

---
+++ ArrayManipulation/Controllers/ArrayCalc.cs
@@ -0,0 +1,27 @@
+﻿using ArrayManipulation.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArrayManipulation.Controllers
+{
+	[Route("api/[controller]")]
+	public class ArrayCalc:Controller
+	{
+		// Reverses an array
+		[Route("reverse")]
+		[HttpGet]
+		public int[] Reverse([FromQuery]int[] productIds)
+		{
+			ArrayManipulationServices.Reverse(productIds);
+			return productIds;
+		}
+
+		// Deletes an element at a particular index in the array
+		[Route("deletepart")]
+		[HttpGet]
+		public int[] DeletePart([FromQuery]int position, [FromQuery]int[] productIds)
+		{
+			ArrayManipulationServices.DeleteElement(ref productIds, position);
+			return productIds;
+		}
+	}
+}
---
+++ ArrayManipulation/Services/ArrayManipulationServices.cs
@@ -0,0 +1,69 @@
+﻿namespace ArrayManipulation.Services
+{
+	/// <summary>
+	/// Service class with helper methods to manipulate arrays
+	/// </summary>
+	public class ArrayManipulationServices
+	{
+		/// <summary>
+		/// Reverses entire array
+		/// </summary>
+		/// <param name="arr"></param>
+		public static void Reverse(int[] arr)
+		{
+			ReverseSection(arr, 0, arr.Length - 1);
+		}
+
+		/// <summary>
+		/// Reverses a section of an array
+		/// </summary>
+		/// <param name="arr"></param>
+		/// <param name="start"></param>
+		/// <param name="end"></param>
+		public static void ReverseSection(int[] arr, int start, int end)
+		{
+			int temp;
+			if (start >= end)
+				return;
+
+			temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+
+			ReverseSection(arr, start + 1, end - 1);
+		}
+
+		/// <summary>
+		/// Deletes element at a particular index in an array
+		/// </summary>
+		/// <param name="arr"></param>
+		/// <param name="position"></param>
+		/// <returns></returns>
+		public static bool DeleteElement(ref int[] arr, int position)
+		{
+			// Check if position is valid
+			if (position < 1 || position > arr.Length)
+			{
+				return false;
+			}
+
+			int indexToDelete = position - 1;
+			int[] newArr = new int[arr.Length - 1];
+
+			// Copying array, removing element
+			for (int i = 0, j = 0; j < newArr.Length; i++, j++)
+			{
+				if (i == indexToDelete)
+				{
+					j--;
+					continue;
+				}
+				newArr[j] = arr[i];
+			}
+
+			arr = newArr;
+
+			return true;
+		}
+	}
+}
